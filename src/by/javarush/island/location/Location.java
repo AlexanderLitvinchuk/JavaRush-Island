3963e3987a92ffc157f1af48b2882d819fa50129
AlexLi <litvinchuk.alexander7@gmail.com>
2022-07-03 16:01:19 +0300

    Доработан класс по созданию острова, добавлено заполнение животными и растениями

--- src/by/javarush/island/location/Location.java
+++ src/by/javarush/island/location/Location.java
@@ -1,5 +1,9 @@
 package by.javarush.island.location;
 
+import by.javarush.island.animal.AnimalEnum;
+import by.javarush.island.animal.AnimalFactory;
+import by.javarush.island.animal.characteristic.Characteristic;
+import by.javarush.island.animal.characteristic.CharacteristicDto;
 import by.javarush.island.cell.Cell;
 
 /**
@@ -12,8 +16,9 @@ public class Location {
 
     private final Cell[][] island = new Cell[HEIGHT][WIDTH];
 
-    public Location() {
+    public Location(Characteristic characteristic) {
         init();
+        initAnimalAndPlant(characteristic);
     }
 
     /**
@@ -26,9 +31,29 @@ public class Location {
                 island[i][j] = new Cell();
             }
         }
-
         System.out.println("Инициализация поля island завершена");
     }
+
+    private void initAnimalAndPlant(Characteristic characteristic) {
+        for (Cell[] cells : island) {
+            for (Cell cell : cells) {
+                initAnimal(cell, characteristic);
+                int amountPlantsInCell = (int) (Math.random() * 10000);
+                cell.getPlants().setAmount(amountPlantsInCell);
+            }
+        }
+    }
+
+    private void initAnimal(Cell cell, Characteristic characteristic) {
+        for (AnimalEnum value : AnimalEnum.values()) {
+            CharacteristicDto characteristicDto = characteristic.getCharacteristicDto(value);
+            int amountAnimalInCell = (int) (Math.random() * characteristicDto.getMaximumAmount());
+            for (int i = 0; i < amountAnimalInCell; i++) {
+                cell.getAnimals().add(AnimalFactory.getAnimal(value));
+            }
+        }
+    }
+
 
     public Cell[][] getIsland() {
         return island;
