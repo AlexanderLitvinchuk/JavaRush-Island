fecdde352d02559c17ef61bbdd790bcadb01b877
AlexLi <litvinchuk.alexander7@gmail.com>
2022-07-03 12:12:06 +0300

    Доработан Animal.java

--- src/by/javarush/island/animal/Animal.java
+++ src/by/javarush/island/animal/Animal.java
@@ -1,6 +1,23 @@
 package by.javarush.island.animal;
 
+import by.javarush.island.animal.directions.DirectionsDrivingEnum;
+
 public abstract class Animal {
+
+    /**
+     * Покушать
+     */
+    public abstract void eat();
+
+    /**
+     * Размножение
+     */
+    public abstract void multiply();
+
+    /**
+     *
+     */
+    public abstract DirectionsDrivingEnum chooseDirectionOfTravel();
 
     public abstract AnimalEnum getAnimalType();
 }
