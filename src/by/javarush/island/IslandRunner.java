122fd2385cdde706a7488281fd37b6b2e7e6d31f
AlexLi <litvinchuk.alexander7@gmail.com>
2022-07-03 17:49:49 +0300

    IslandRunner обновлен для теста

--- src/by/javarush/island/IslandRunner.java
+++ src/by/javarush/island/IslandRunner.java
@@ -1,15 +1,35 @@
 package by.javarush.island;
 
+import by.javarush.island.animal.chances.FoodChances;
+import by.javarush.island.animal.characteristic.Characteristic;
 import by.javarush.island.location.Location;
+import by.javarush.island.location.LocationPrint;
 
 public class IslandRunner {
 
     public static void main(String[] args) {
 
-        Location location = new Location();
+        FoodChances foodChances = new FoodChances();
+        Characteristic characteristic = new Characteristic();
+        Location location = new Location(characteristic);
 
+        LocationPrint.print(location.getIsland());
+
+
+        for (int i = 0; i < 100; i++) {
+            goTact(location);
+
+        }
 
         System.out.println("Test");
 
     }
+
+    private static void goTact(Location location) {
+        // Каждая клетка в своём потоке смотрит, кто кого может сожрать
+        // Голодные умирают
+        // Смотрим, если не голодный - размножаемся
+        // Если голодный - переход
+    }
+
 }
