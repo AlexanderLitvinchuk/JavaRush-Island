cc41e74b1fd1ec071a3861f8f8a05d3f8d587d0c
AlexLi <litvinchuk.alexander7@gmail.com>
2022-07-04 23:32:54 +0300

    Доработан клас Растения Plants.java

--- src/by/javarush/island/plants/Plants.java
+++ src/by/javarush/island/plants/Plants.java
@@ -2,23 +2,43 @@ package by.javarush.island.plants;
 
 /**
  * Растения
-  */
+ */
 public class Plants {
 
     /**
      * Количество
      */
-    private int amount = 0;
+    private double amount = 0;
 
-    public int getAmount() {
+    public double getAmount() {
         return amount;
     }
 
-    public void setAmount(int amount) {
+    public void setAmount(double amount) {
         this.amount = amount;
     }
 
-    public void reduceAmount(int i) {
+    /**
+     * Уменьшение количества растений
+     *
+     * @param i - количество употребляемых растений, на которое будет уменьшено
+     * @return Возвращает true когда есть достаточное количество растений, false - когда недостаточно
+     */
+    public boolean reduceAmount(double i) {
         amount = amount - i;
+        if (amount < 0) {
+            amount = 0;
+            return false;
+        }
+
+        return true;
     }
+
+    /**
+     * Увеличивает количество растений на 5%
+     */
+    public void plantsGrewByFivePercent() {
+        amount = amount + (amount / 100 * 5);
+    }
+
 }
