3cc19fa0adb7d60da309fb2fd23c1cd6b14d0555
AlexLi <litvinchuk.alexander7@gmail.com>
2022-07-07 23:01:06 +0300

    CellTask для каждой клетки в 1 ход добавлено удаление не живых животных, размножение животных

--- src/by/javarush/island/animal/task/CellTask.java
+++ src/by/javarush/island/animal/task/CellTask.java
@@ -1,6 +1,15 @@
 package by.javarush.island.animal.task;
 
+import by.javarush.island.animal.Animal;
+import by.javarush.island.animal.AnimalEnum;
+import by.javarush.island.animal.AnimalFactory;
+import by.javarush.island.animal.characteristic.Characteristic;
+import by.javarush.island.animal.characteristic.CharacteristicDto;
 import by.javarush.island.cell.Cell;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CellTask implements Runnable {
 
@@ -16,5 +25,46 @@ public class CellTask implements Runnable {
         cell.getPlants().plantsGrewByFivePercent();
         // каждое животное кушает
         cell.getAnimals().forEach(animal -> animal.eat(cell));
+        //  Поиск животные которые не живые
+        List<Animal> notLiveAnimalList = cell.getAnimals().stream()
+                .filter(animal -> !animal.isLive())
+                .collect(Collectors.toList());
+        // Удаляем не живых животных
+        cell.getAnimals().removeAll(notLiveAnimalList);
+
+        // Размножение животных
+        // Подсчет сколько животных готово к размножению
+        Map<AnimalEnum, Long> countMultiply = cell.getAnimals().stream()
+                .filter(Animal::multiply)
+                .collect(Collectors.groupingBy(
+                        Animal::getAnimal,
+                        Collectors.counting()
+                ));
+
+        // Сколько животных в клетке
+        Map<AnimalEnum, Long> countAnimal = cell.getAnimals().stream()
+                .collect(Collectors.groupingBy(
+                        Animal::getAnimal,
+                        Collectors.counting()
+                ));
+
+        // для каждого вида животных
+        countMultiply.forEach((animalEnum, aLong) -> {
+            // получаем информацию настроек животных
+            CharacteristicDto characteristicDto = Characteristic.getCharacteristicDto(animalEnum);
+            // проходим по каждому второму животному (2 животных порождают одного нового)
+            for (int i = 0; i < aLong / 2; i++) {
+                // общее количество в клетке
+                Long aLong1 = countAnimal.get(animalEnum);
+                // пока в клетке есть место для нового животного - создаем и помещаем к животным
+                if (characteristicDto.getMaximumAmount() > aLong1) {
+                    cell.getAnimals().add(AnimalFactory.getAnimal(animalEnum));
+                    countAnimal.put(animalEnum, ++aLong1);
+                } else {
+                    // мест нет - завершаем цикл для этого типа животных
+                    break;
+                }
+            }
+        });
     }
 }
