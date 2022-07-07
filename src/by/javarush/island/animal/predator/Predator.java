c103169a11d746eba76e2202e9e4e31768d3334b
AlexLi <litvinchuk.alexander7@gmail.com>
2022-07-08 00:32:36 +0300

    Predator реализован метод кушать для хищника

--- src/by/javarush/island/animal/predator/Predator.java
+++ src/by/javarush/island/animal/predator/Predator.java
@@ -1,9 +1,16 @@
 package by.javarush.island.animal.predator;
 
 import by.javarush.island.animal.Animal;
+import by.javarush.island.animal.AnimalEnum;
 import by.javarush.island.animal.AnimalTypeEnum;
+import by.javarush.island.animal.characteristic.Characteristic;
 import by.javarush.island.animal.directions.DirectionsDrivingEnum;
 import by.javarush.island.cell.Cell;
+import by.javarush.island.utils.Utils;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public abstract class Predator extends Animal {
 
@@ -14,31 +21,47 @@ public abstract class Predator extends Animal {
 
     @Override
     public void eat(Cell cell) {
+        // Predator кушает
+        boolean isAteFull;
+        Set<AnimalEnum> animalEnums = getFoodChances().keySet();
+        // получаем список живых животных которых мы можем скушать
+        List<Animal> collect = cell.getAnimals().stream()
+                .filter(Animal::isLive)
+                .filter(animal -> animalEnums.contains(animal.getAnimal()))
+                .collect(Collectors.toList());
+        double energy = getCharacteristic().getEnergy();
 
+        for (Animal animal : collect) {
+            // Тип животного, который мы кушаем
+            AnimalEnum animalEnum = animal.getAnimal();
+
+            boolean randomBoolean = Utils.getRandomBoolean(getFoodChances().get(animalEnum));
+            if (randomBoolean) {
+                // Вес животного, который мы кушаем
+                double weight = Characteristic.getCharacteristicDto(animalEnum).getWeight();
+                energy = energy - weight;
+                // Помечаем что животное скушано
+                animal.killAnimal();
+            }
+
+            // Когда съели достаточно - выходим из цикла
+            if (energy <= 0)
+                break;
+        }
+
+        // Если животными не наелись, кушаем траву
+        if (energy > 0) {
+            isAteFull = cell.getPlants().reduceAmount(energy);
+        } else
+            isAteFull = true;
+
+        // Проверяем наелось животное - сбросили счётчик голода, иначе добавляем + 1 ход
+        if (isAteFull) {
+            resetHungerCounter();
+        } else {
+            addOneTackHunger();
+        }
     }
-
-//    @Override
-//    public void eatV2(Cell cell) {
-//        FoodChances foodChances = new FoodChances();
-//
-//        AnimalEnum animalEnum = getAnimal();
-//        CharacteristicDto characteristicDto = Characteristic.getCharacteristicDto(animalEnum);
-//        Map<AnimalEnum, Integer> animalEnumIntegerMap = foodChances.getAnimalEnumMapMap().get(animalEnum);
-//        List<Animal> животныеКоторыеМожноСкушать = cell.getAnimals().stream()
-//                .filter(animal -> animalEnumIntegerMap.get(animal.getAnimal()) != 0)
-//                .collect(Collectors.toList());
-//
-//        if (животныеКоторыеМожноСкушать.size() == 0) {
-//            addOneTackHunger();
-//        } else {
-//            double energy = characteristicDto.getEnergy();
-//            double скушал = 0;
-//            while (скушал < energy) {
-//                животныеКоторыеМожноСкушать.
-//            }
-//
-//        }
-//    }
 
     @Override
     public DirectionsDrivingEnum chooseDirectionOfTravel() {
