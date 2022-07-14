da6b3cdde74d31760e01059f1406bda2fb9a6765
AlexLi <litvinchuk.alexander7@gmail.com>
2022-07-14 23:06:25 +0300

    Оптимизирован IslandRunner

--- src/by/javarush/island/IslandRunner.java
+++ src/by/javarush/island/IslandRunner.java
@@ -1,41 +1,27 @@
 package by.javarush.island;
 
-import by.javarush.island.animal.chances.FoodChances;
 import by.javarush.island.animal.task.LocationTask;
 import by.javarush.island.location.Location;
 import by.javarush.island.location.LocationPrint;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-
 public class IslandRunner {
 
-    private static final int STEP_COUNT = 2;
+    private static final int STEP_COUNT = 20;
 
     public static void main(String[] args) {
 
-        FoodChances foodChances = new FoodChances();
         // Создаём остров
         Location location = new Location();
         // печатаем остров
         LocationPrint.print(location.getIsland());
 
-        // Наверное, тут нужно переделать на обычный forEach
-//        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        // Запуск ходов на острове
         for (int i = 0; i < STEP_COUNT; i++) {
-//            executorService.submit(new LocationTask(location));
             new LocationTask(location).run();
         }
 
-        System.out.println("Test");
+        System.out.println("Время прошло покинуть остров !!!");
 
-    }
-
-    private static void goTact(Location location) {
-        // Каждая клетка в своём потоке смотрит, кто кого может сожрать
-        // Голодные умирают
-        // Смотрим, если не голодный - размножаемся
-        // Если голодный - переход
     }
 
 }
