daff4f8932ec6b4e23359dc3fdcbf1988e22906a
AlexLi <litvinchuk.alexander7@gmail.com>
2022-07-14 23:46:44 +0300

    IslandRunner Доработан вывод информации старта и завершения расчета хода

--- src/by/javarush/island/IslandRunner.java
+++ src/by/javarush/island/IslandRunner.java
@@ -17,7 +17,9 @@ public class IslandRunner {
 
         // Запуск ходов на острове
         for (int i = 0; i < STEP_COUNT; i++) {
+            System.out.printf("Расчёт хода номер = %d НАЧАТ", i + 1);
             new LocationTask(location).run();
+            System.out.printf("Расчёт хода номер = %d ЗАВЕРШЁН", i + 1);
         }
 
         System.out.println("Время прошло покинуть остров !!!");
